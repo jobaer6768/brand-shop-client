e3c6dc65381ea11820cb2920a7725a14668ee820
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-10-24 12:26:35 +0600

    used slide-show in products section

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Product from "./Product";
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 
 const Products = () => {
@@ -8,10 +10,34 @@ const Products = () => {
     const loadedProducts = useLoaderData();
     const [products, setProducts] = useState(loadedProducts);
 
+    const divStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundSize: '680px',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        height: '80vh'
+    }
+
+
+
     return (
         <div className="container mx-auto my-20">
 
-            <div className="text-6xl my-10">Slider</div>
+            <div className="text-center text-4xl font-bold mb-6">Our Featured Products</div>
+
+            <div className="">
+                <Slide>
+                    {products.map((product, index) => (
+                        <div key={index}>
+                            <div className="relative" style={{ ...divStyle, 'backgroundImage': `url(${product.image})` }}>
+                                <span className="absolute bottom-0 text-xs opacity-80">{product.name}</span>
+                            </div>
+                        </div>
+                    ))}
+                </Slide>
+            </div>
             <div className="grid md:grid-cols-2 gap-8">
                 {
                     products.map(product => <Product
